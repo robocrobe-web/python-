c4fdd3d7da6b675edf625f7d81a708ccf914c2c5
robocrobe-web <robocrobe@gmail.com>
2026-06-13 13:05:16 +0530

    Update student_marks_calculator.py

--- student_marks_calculator.py
+++ student_marks_calculator.py
@@ -1,1 +1,51 @@
+import pandas as pd   
+url = "https://github.com/robocrobe-web/python-/blob/main/student.csv"
+df = pd.read_csv(url) 
 
+df["total"] = df["Physics"] + df["Chemistry"] + df["Maths"] 
+
+df["percentage"] = df["total"] / 300 
+
+def grade_function(percentage): 
+    if percentage >= 90: 
+        return "A+" 
+    elif percentage >= 80:
+        return "A"
+    elif percentage >= 70:
+        return "B"
+    elif percentage >= 60: 
+        return "C"
+    else:
+        return "F" 
+         
+df["grade"] = df["percentage"].apply(get_grades)
+
+
+df["Rank"] = (
+    df["Percentage"]
+    .rank(ascending=False, method="dense")
+    .astype(int)
+)
+
+df = df.sort_values(by="Rank")
+
+topper = df.iloc[0]
+
+
+print(df)
+
+
+print(f"Name: {topper['Name']}") 
+print(f"Percentage: {topper['Percentage']:.2f}%")
+print(f"Grade: {topper['Grade']}")
+ 
+
+
+print(f"Physics: {df['Physics'].mean():.2f}")
+print(f"Chemistry: {df['Chemistry'].mean():.2f}")
+print(f"Maths: {df['Maths'].mean():.2f}")
+
+
+df.to_csv("student_report.csv", index=False)
+
+print("Report saved as 'student_report.csv'") 
